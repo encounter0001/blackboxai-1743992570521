6530dfc43f2ef9f6e22ddecedba91b04c5c8f467
encounter0001 <129878982+encounter0001@users.noreply.github.com>
2025-04-07 08:55:20 +0530

    Add tailwind.config.js 1743996319699

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./*.html",
+    "./**/*.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        wyno: {
+          primary: '#4F46E5',
+          danger: '#EF4444', 
+          success: '#10B981'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
